56374410600c80c3e92c536dd2d6b04de7aa0cd0
LeonardChi季言勵 <35521@oitc.com.tw>
2025-07-10 16:15:56 +0800

    Add develop watch mode to chat-ui

--- chat-ui/constants/chat.ts
+++ chat-ui/constants/chat.ts
@@ -4,16 +4,16 @@ export const USER = {
   name: 'You',
   avatarUrl: 'https://img.daisyui.com/images/profile/demo/anakeen@192.webp',
   position: 'end',
-};
+} as const
 
 export const AI = {
   name: 'ChatBot',
   avatarUrl: 'https://img.daisyui.com/images/profile/demo/kenobee@192.webp',
   position: 'start',
-};
+} as const
 
 export const SYSTEM = {
   name: 'System',
   avatarUrl: 'https://img.daisyui.com/images/profile/demo/kenobee@192.webp',
   position: 'start',
-};
+} as const
